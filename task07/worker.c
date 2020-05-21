5bbb7f78d1b21957f6d85def268541522fc4a195
Archibaldo44 <Archibaldo44@gmail.com>
2020-05-21 23:21:25 +0200

    Update worker.c

--- task07/worker.c
+++ task07/worker.c
@@ -4,47 +4,92 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>     // strlen() memset()
-#include <unistd.h>
+#include <errno.h>      // errno
+#include <string.h>     // strlen()
+#include <unistd.h>     // write()
 #include <sys/types.h>  // open()
 #include <sys/stat.h>   // open()
 #include <fcntl.h>      // open() close()
+#include "globals.h"
 
-int main(int argc, char** argv)
+/* prints error message and terminates the program */
+void error_exit(char*);
+
+int main(void)
 {
-    pid_t pid = getpid();
-    int fd;
-    
-    if (argc != 3) {
-        fprintf(stderr, "----Worker(%d): wrong number of arguments\n", pid);
-        exit(EXIT_FAILURE);
-    }
-    int sleep_time = 0;
-    if (!sscanf(argv[1], "%d", &sleep_time) || (sleep_time < 0)) {
-        fprintf(stderr, "----Worker(%d): argument is wrong.\n", pid);
-        exit(EXIT_FAILURE);
-    }
-    
-    /* open the named pipe for writing*/
-    fd = open(argv[2], O_WRONLY);
-    if (fd < 0) {
-        perror("open");
-        exit(EXIT_FAILURE);
-    }
-    
-    /* write something into the named pipe */
-    char str[80] = { 0 };
-    sprintf(str, "Worker(%d): going to do smth for %s sec.\n", pid, argv[1]);
-    write(fd, str, strlen(str) + 1); 
+	// check if boss FIFO file exists
+	if (access(BOSS_FIFO, F_OK) < 0 ) {
+		error_exit("Boss FIFO file doesn't exist");
+	}
 
-    sleep(sleep_time);
+	pid_t pid = getpid();                               // process pid
+	char worker_fifo_path[WORKER_FIFO_NAME_LEN]= {0};  // to keep full path to FIFO file
+	snprintf(worker_fifo_path, WORKER_FIFO_NAME_LEN, WORKER_FIFO_TEMPLATE, getpid());
+	
+	// create worker FIFO file, if file already exits - delete it
+    unlink(worker_fifo_path);
+	umask(0);
+	if (mkfifo(worker_fifo_path, 0777) < 0) {
+        error_exit("mkfifo");
+    }
 
-    memset(str, '\0', 80);
-    sprintf(str, "Worker(%d): terminating.\n", pid);
-    write(fd, str, strlen(str) + 1); 
-    
-    /* close the named pipe */
-    close(fd); 
+	// open BOSS_FIFO (outgoing)
+    int boss_fifo = open(BOSS_FIFO, O_WRONLY);
+    if (boss_fifo < 0) {
+        error_exit("Failed to open outgoing FIFO");
+    }
+
+	// send pid to boss as "Connect(12345)"
+	char buffer[BUFSIZE] = { 0 };
+	snprintf(buffer, BUFSIZE, "Connect(%d)", pid);
+	if (write(boss_fifo, buffer, BUFSIZE) < 0) {
+		error_exit("Failed to write to boss FIFO");
+	}
+
+	// open worker FIFO
+	int worker_fifo = open(worker_fifo_path, O_RDONLY);
+	if (worker_fifo < 0) {
+        error_exit("Failed to open worker FIFO");
+    }
+
+	// wait for smth from boss (read from boss FIFO)
+	memset(buffer, '\0', BUFSIZE);
+	if (read(worker_fifo, buffer, BUFSIZE) < 0) { 
+		// smth wrong happend; probably FIFO was closed
+		error_exit("Failed to read from incoming FIFO");
+    }
+	
+	printf("Worker(%d) received data from boss = '%s'\n", pid, buffer);
+	sleep(5);
+	
+	// send exit message to boss. "Done(12345)"
+	memset(buffer, '\0', BUFSIZE);
+	snprintf(buffer, BUFSIZE, "Done(%d)", pid);
+	if (write(boss_fifo, buffer, BUFSIZE) < 0) {
+		error_exit("write");
+	}
+	
+	// close boss FIFO
+	if (close(boss_fifo) < 0) {
+		error_exit("Failed to close boss FIFO");
+	}
+	
+	// close worker FIFO
+	if (close(worker_fifo) < 0) {
+		error_exit("Failed to close worker FIFO");
+	} 
+	
+	// delete FIFO file
+	if (unlink(worker_fifo_path) < 0) {
+        error_exit("Failed to delete worker FIFO file");
+    }
 
     exit(EXIT_SUCCESS);
+}
+
+/* prints error message and terminates the program */
+void error_exit(char* msg)
+{
+    fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+    exit(EXIT_FAILURE);
 }
